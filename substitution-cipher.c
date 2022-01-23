05618443d385cb4f336ebb24bbdd7ea05d2ee74f
benwade91 <70988204+benn925@users.noreply.github.com>
2022-01-22 20:10:14 -0800

    algorithm catches repeating characters in key

--- substitution-cipher.c
+++ substitution-cipher.c
@@ -5,10 +5,31 @@
 int main(int argc, char *argv[])
 {
     string cipherKey = argv[1];
-    if (strlen(cipherKey) == 26)
+    string cipherError;
+    int keyLength = strlen(cipherKey);
+
+    for (int i = 0; i < keyLength; i++)
+    {
+        for (int j = i+1; j < keyLength; j++)
+        {
+            if (cipherKey[i] == cipherKey[j])
+            {
+                cipherError = "No repeating characters allowed in key\n";
+            }
+            
+        }
+        
+    }
+    
+    if (strlen(cipherError) > 1)
+    {
+        printf("%s", cipherError);
+    }
+    else if (keyLength == 26)
     {
     printf("%s\n", argv[1]);
     }
+    
     else
     {
         printf("oops");
